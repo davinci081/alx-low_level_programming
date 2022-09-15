91787737860aa40f8c4fd6ca58b197b83d35d498
Ani vincent <nkemakonamvincent25@gmail.com>
2022-09-15 07:24:07 -0700

    fixed task 4,6,7,9

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,6 +10,8 @@ void print_line(int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		_putchar('95');
+	{
+		_putchar(95);
+	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
